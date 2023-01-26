4673e6d9347b34d4de1cc6ce950c0d96455cb5ca
Trevor DePew <t.depewmusic@gmail.com>
2023-01-26 17:16:00 -0500

    html elements and css

--- synth-frontend/src/components/Interface.jsx
+++ synth-frontend/src/components/Interface.jsx
@@ -15,25 +15,34 @@ const Interface = () => {
             <div className="fader-label">
               <p>Volume</p>
             </div>
-            <input type="range" min="0" max="127" value="60" class="slider" id="volume-fader">
-
-            </input>
+            <div id="volume-fader">
+              <input type="range" id="volume" name="volume" min="0" max="127" className="fader" />
+              <datalist id="values">
+                <option value="100" label="100"></option>
+                <option value="50" label="50"></option>
+                <option value="0" label="0 db"></option>
+              </datalist>
+            </div>
           </div>
         </div>
         <div id="center-container">
-          <p>Waveform Select</p>
           <div id="waveform-select-container">
-            <div className="button-container">
-              <button>Sine</button>
+            <div className="fader-label">
+              <p>Waveform Select</p>
             </div>
-            <div className="button-container">
-              <button>Square</button>
-            </div>
-            <div className="button-container">
-              <button>Sawtooth</button>
-            </div>
-            <div className="triangle">
-              <button>Triangle</button>
+            <div id="buttons">
+              <div className="button-container">
+                <button className="waveselect">Sine</button>
+              </div>
+              <div className="button-container">
+                <button className="waveselect">Square</button>
+              </div>
+              <div className="button-container">
+                <button className="waveselect">Sawtooth</button>
+              </div>
+              <div className="button-container">
+                <button className="waveselect">Triangle</button>
+              </div>
             </div>
           </div>
         </div>
@@ -42,9 +51,14 @@ const Interface = () => {
             <div className="fader-label">
               <p>LoPass Filter</p>
             </div>
-            <input type="range" min="0" max="127" value="60" class="slider" id="lopass-fader">
-
-            </input>
+            <div id="volume-fader">
+              <input type="range" id="lopass" name="lopass" min="0" max="127" className="fader" />
+              <datalist id="values">
+                <option value="15000" label="15000"></option>
+                <option value="7400" label="7400"></option>
+                <option value="200" label="200 Hz"></option>
+              </datalist>
+            </div>
           </div>
         </div>
       </div>
